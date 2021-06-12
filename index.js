18061ed1f85621543209d8f778545fd75ddb3f19
harshsahay0212 <harshsahay0212@gmail.com>
2021-06-12 09:55:42 +0530

    ok

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function myrelationship() {
     // alert('humko koi click kiya');
     const para1 = document.getElementById('section1-para1');
-    para1.innerHTML = 'Darsh is my younger brother , he is very naughty !!!';
+    para1.innerHTML = 'I am monkey and Darsh is my younger brother , he is very naughty !!!';
 }
